46ac963c4c372a08124a82c3a138afa287c9ebf4
mirsahib <mirsahib24@gmail.com>
2019-12-20 17:21:08 +0600

    admin.js backend rewritten with proper aggregation admin.js frontend is rewritten with less and structure code comment is not done

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,471 +1,240 @@
-var data = JSON.parse(localStorage.getItem("admission"));
-var dataLength = data.length;
-var schoolName;
-//var majorName;
-var span = {
-  "Last 1 Year": [2019, 2018],
-  "Last 2 Year": [2019, 2018, 2017],
-  "Last 3 Year": [2019, 2018, 2017, 2018]
-};
-//initializing chart (global)
-var majorChart;
-var yearChart;
-var schoolChart;
-$(document).ready(function() {
-  //load dataset
-  if (localStorage.length == 0) {
-    $.ajax({
-      type: "GET",
-      //data: { get_param: "value" },
-      url: "/operator/data",
-      dataType: "json", // <-- add this
-      success: function(data) {
-        localStorage.setItem("admission", JSON.stringify(data));
-        console.log("database stored in local storage");
-      },
-      error: function(data) {
-        //console.log("error");
-        console.log(data);
-      }
-    });
-  }
+// var ctx = $("#SchoolChart");
+// var chart = new Chart(ctx, {});
 
-  schoolName = getSchoolName();
-  //majorName = getMajorName();
-  //populate school name
-  for (var i = 0; i < schoolName.length; i++) {
-    $("#schoolName").append(
-      '<option value="' + schoolName[i] + '">' + schoolName[i] + "</option>"
-    );
-    $("#majorName").append(
-      '<option value="' + schoolName[i] + '">' + schoolName[i] + "</option>"
-    );
-  }
-
-  //year wise distribution event
-  $("#yearSubmit").click(function() {
-    var year = $("#yearSelect option:selected").text();
-    var semester = $("#semesterSelect option:selected").text();
-
-    if (year == "Select Year" || semester == "Select Semester") {
-      alert("Please select a valid year or semester");
-    } else {
-      generateYearGraph(year, semester);
+$("#semSubmit").click(function() {
+  var strYear = $("#strYear_sem option:selected").text();
+  var endYear = $("#endYear_sem option:selected").text();
+  strYear = Number(strYear);
+  endYear = Number(endYear);
+  console.log(strYear + " " + endYear);
+  var data = { str: strYear, end: endYear };
+  $.ajax({
+    type: "GET",
+    data: data,
+    url: "/admin/semester",
+    dataType: "json", // <-- add this
+    success: function(data) {
+      console.log(data);
+      processSemData(strYear, endYear, data);
+      //localStorage.setItem("semester", JSON.stringify(data));
+    },
+    error: function(data) {
+      //console.log("error");
+      console.log(data);
     }
-  });
-
-  //Schoolwise distribution event
-  $("#schoolSubmit").click(function() {
-    var school_name = $("#schoolName option:selected").text();
-    var yearSpan = $("#yearSpan option:selected").text();
-
-    if (school_name == "Select School Name" || yearSpan == "Select Year Span") {
-      alert("Please select a valid year or semester");
-    } else {
-      generateSchoolGraph(span[yearSpan], school_name);
-    }
-  });
-  //Majorwise distribution event
-  $("#majorSubmit").click(function() {
-    var major_name = $("#majorName option:selected").text();
-    var majorYearSpan = $("#majorYearSpan option:selected").text();
-    if (major_name == "Select School Name" || yearSpan == "Select Year Span") {
-      alert("Please select a valid year or semester");
-    } else {
-      generateMajorGraph(span[majorYearSpan], major_name);
-    }
-  });
-  //generate report event
-  $("#generateReport").click(function() {
-    // get size of report page
-    var reportPageHeight = 2000;
-    var reportPageWidth = window.innerWidth;
-
-    // create a new canvas object that we will populate with all other canvas objects
-    var pdfCanvas = $("<canvas />").attr({
-      id: "canvaspdf",
-      width: reportPageWidth,
-      height: reportPageHeight
-    });
-
-    // keep track canvas position
-    var pdfctx = $(pdfCanvas)[0].getContext("2d");
-    var pdfctxX = 0;
-    var pdfctxY = 0;
-    var buffer = 100;
-
-    // for each chart.js chart
-    $("canvas").each(function(index) {
-      // get the chart height/width
-      var canvasHeight = $(this).innerHeight();
-      var canvasWidth = $(this).innerWidth();
-      // draw the chart into the new canvas
-      pdfctx.drawImage($(this)[0], pdfctxX, pdfctxY, canvasWidth, canvasHeight);
-      pdfctxY += canvasHeight;
-
-      // our report page is in a grid pattern so replicate that in the new canvas
-      //if (index % 2 === 1) {
-      //  pdfctxX = 0;
-      // pdfctxY += canvasHeight + buffer;
-      //}
-    });
-
-    // create new pdf and add our new canvas as an image
-    var pdf = new jsPDF("l", "pt", [reportPageWidth, reportPageHeight]);
-    pdf.addImage($(pdfCanvas)[0], "PNG", reportPageWidth / 2, 0);
-
-    var time = new Date();
-    var date = time.getDate();
-    var month = time.getMonth();
-    var min = time.getMinutes();
-    var hour = time.getHours();
-    // download the pdf
-    pdf.save(min + "_" + hour + "_" + date + "_" + month + ".pdf");
   });
 });
-function getSchoolName() {
-  var container = [];
-  for (let i = 0; i < dataLength; i++) {
-    if (data[i].Year == "2019") {
-      container.push(data[i].School);
-    }
-  }
-  container = container.filter((item, i, ar) => ar.indexOf(item) === i);
-  return container;
-}
-//don't this function
-function getMajorName() {
-  var container = [];
-  for (let i = 0; i < dataLength; i++) {
-    if (data[i].Year == "2019") {
-      container.push(data[i].Major);
-    }
-  }
-  container = container.filter((item, i, ar) => ar.indexOf(item) === i);
-  return container;
-} //this function is irrelevant to the project
 
-function generateYearGraph(year, semester) {
-  //console.log("generate graph");
-  var container = [];
-  for (let i = 0; i < dataLength; i++) {
-    Year = data[i].Year;
-    Semester = data[i].Semester;
-    Semester = Semester.split(" ");
-    if (Year == year && Semester[0] == semester) {
-      container.push(data[i]);
-    }
-  }
-  var result = yearDistribution(container);
-  showYearChart(result, schoolName, "pie", "% of Student"); //showChart(result,label,chartType,chartLegend)
-}
-
-function yearDistribution(data) {
-  var array = Array.from(Array(schoolName.length), () => 0);
-  var totalStudent = 0;
-  for (let i = 0; i < schoolName.length; i++) {
-    for (let j = 0; j < data.length; j++) {
-      if (schoolName[i] == data[j].School) {
-        array[i] += data[j].Students;
-        totalStudent += data[j].Students;
-      }
-    }
-  }
-  for (var i = 0; i < schoolName.length; i++) {
-    array[i] = ((array[i] / totalStudent) * 100).toFixed(2);
-  }
-
-  return array;
-}
-function generateSchoolGraph(yearList, name) {
-  var container = [];
-  for (let i = 0; i < yearList.length; i++) {
-    var yearContainer = [];
-    for (let j = 0; j < dataLength; j++) {
-      if (data[j].Year == yearList[i] && data[j].School == name) {
-        yearContainer.push(data[j]);
-      }
-    }
-    container.push(yearContainer);
-  }
-  var result = semesterDistribution(container);
-  showSchoolChart(yearList, result);
-}
-
-function semesterDistribution(yearList) {
-  var yearSemDist = [];
-
-  for (var i = 0; i < yearList.length; i++) {
-    var semContainer = { Autumn: 0, Spring: 0, Summer: 0 };
-    var totalStudent = 0;
-    //year span
-    for (var j = 0; j < yearList[i].length; j++) {
-      //object in a year
-      semesterName = yearList[i][j].Semester;
-      semesterName = semesterName.split(" ");
-      semesterName.pop();
-      totalStudent += yearList[i][j].Students;
-      if (semesterName == "Autumn") {
-        semContainer["Autumn"] += yearList[i][j].Students;
-      } else if (semesterName == "Spring") {
-        semContainer["Spring"] += yearList[i][j].Students;
-      } else {
-        semContainer["Summer"] += yearList[i][j].Students;
-      }
-    }
-    semContainer["Autumn"] = (
-      (semContainer["Autumn"] / totalStudent) *
-      100
-    ).toFixed(2);
-    semContainer["Spring"] = (
-      (semContainer["Spring"] / totalStudent) *
-      100
-    ).toFixed(2);
-    semContainer["Summer"] = (
-      (semContainer["Summer"] / totalStudent) *
-      100
-    ).toFixed(2);
-    yearSemDist.push(semContainer);
-  }
-  return yearSemDist;
-}
-
-function showYearChart(data, label, chartType, legend) {
-  //get the chart canvas
-  var ctx1 = $("#yearChart");
-  //generate random color
-  var colorList = [];
-  for (let i = 0; i < data.length; i++) {
-    colorList.push(
-      "#" +
-        Math.random()
-          .toString(16)
-          .substr(-6)
-    );
-  }
-
-  //pie chart data
-  var data1 = {
-    labels: label,
-    datasets: [
-      {
-        label: "Year Wise Student %",
-        data: data,
-        backgroundColor: colorList,
-        borderColor: colorList,
-        borderWidth: [1, 1, 1, 1, 1]
-      }
-    ]
-  };
-  //options
-  var options = {
-    responsive: true,
-    title: {
-      display: true,
-      position: "top",
-      text: legend,
-      fontSize: 18,
-      fontColor: "#111"
+$("#schoolSubmit").click(function() {
+  var strYear = $("#strYear_school option:selected").text();
+  var endYear = $("#endYear_school option:selected").text();
+  strYear = Number(strYear);
+  endYear = Number(endYear);
+  console.log(strYear + " " + endYear);
+  var data = { str: strYear, end: endYear };
+  $.ajax({
+    type: "GET",
+    data: data,
+    url: "/admin/school",
+    dataType: "json", // <-- add this
+    success: function(data) {
+      console.log(data);
+      processSchoolData(strYear, endYear, data);
+      //localStorage.setItem("school", JSON.stringify(data));
     },
-    legend: {
-      display: true,
-      position: "bottom",
-      labels: {
-        fontColor: "#333",
-        fontSize: 16
-      }
+    error: function(data) {
+      //console.log("error");
+      console.log(data);
     }
-  };
-  //create Chart class object
-  yearChart = new Chart(ctx1, {
-    type: chartType,
-    data: data1,
-    options: options
   });
-}
+  //processSchoolData(strYear, endYear);
+});
 
-function showSchoolChart(yearList, data) {
-  var ctx1 = $("#SchoolChart");
+$("#majorSubmit").click(function() {
+  let strYear = $("#strYear_major option:selected").text();
+  let endYear = $("#endYear_major option:selected").text();
+  let school = $("#slt_school option:selected").text();
+  strYear = Number(strYear);
+  endYear = Number(endYear);
+  console.log(strYear + " " + endYear + " " + school);
+  let data = { str: strYear, end: endYear, school: school };
+  $.ajax({
+    type: "GET",
+    data: data,
+    url: "/admin/major",
+    dataType: "json", // <-- add this
+    success: function(data) {
+      console.log(data);
+      processMajorData(strYear, endYear, data);
+    },
+    error: function(data) {
+      //console.log("error");
+      console.log(data);
+    }
+  });
+});
 
-  // catagorize data by semseter
-  data = stripData(data);
-  container = [];
-  labelArr = ["Autumn", "Spring", "Summer"];
-  for (var i = 0; i < data.length; i++) {
-    color =
+function processSemData(strYear, endYear, data) {
+  var arr = data.message;
+  var semester = {
+    Autumn: [],
+    Spring: [],
+    Summer: []
+  };
+  var yearList = [];
+  for (let i = endYear; i >= strYear; i--) {
+    yearList.push(i);
+  }
+
+  for (let i = 0; i < arr.length; i = i + 2) {
+    let total = arr[i].total + arr[i + 1].total;
+    let sem = arr[i]._id.Semester.split(" ")[0];
+    semester[sem].push(total);
+  }
+  var graphData = [];
+  const semList = Object.keys(semester);
+  for (let i = 0; i < 3; i++) {
+    let color =
       "#" +
       Math.random()
         .toString(16)
-        .substr(-6); //generate random color
-    dataset = {};
-    dataset.label = labelArr[i];
-    dataset.data = data[i];
-    dataset.backgroundColor = color;
-    dataset.borderColor = color;
-    dataset.borderWidth = [1, 1, 1, 1, 1];
-    container.push(dataset);
-  }
+        .substr(-6);
 
-  //pie chart data
-  var data1 = {
-    labels: yearList,
-    datasets: container
-  };
-  //options
-  var options = {
-    responsive: true,
-    title: {
-      display: true,
-      position: "top",
-      text: "% of Student",
-      fontSize: 18,
-      fontColor: "#111"
-    },
-    legend: {
-      display: true,
-      position: "bottom",
-      labels: {
-        fontColor: "#333",
-        fontSize: 16
-      }
+    container = {
+      label: semList[i],
+      backgroundColor: color,
+      borderColor: color,
+      borderWidth: 1,
+      data: semester[semList[i]]
+    };
+    graphData.push(container);
+  }
+  showGraph(yearList, graphData, "Semester Wise Chart", "#yearChart");
+}
+
+function processSchoolData(strYear, endYear, data) {
+  data = data.message;
+  //school name
+  let schoolList = [];
+  data.forEach(element => {
+    if (schoolList.indexOf(element._id.School) == -1) {
+      schoolList.push(element._id.School);
     }
-  };
-  //create Chart class object
-  schoolChart = new Chart(ctx1, {
-    type: "bar",
-    data: data1,
-    options: options
   });
-}
-
-function stripData(data) {
-  var autumn = [];
-  var spring = [];
-  var summer = [];
-  var container = [];
-  for (let i = 0; i < data.length; i++) {
-    value = Object.values(data[i]);
-    autumn.push(value[0]);
-    spring.push(value[1]);
-    summer.push(value[2]);
+  let yearList = [];
+  for (let i = endYear; i >= strYear; i--) {
+    yearList.push(i);
   }
-  container.push(autumn);
-  container.push(spring);
-  container.push(summer);
-  return container;
+  let dataset = {};
+  for (let i = 0; i < schoolList.length; i++) {
+    dataset[schoolList[i]] = [];
+  }
+  data.forEach(element => {
+    let total = element.total;
+    let school = element._id.School;
+    dataset[school].push(total);
+  });
+  let len = schoolList.length;
+  let graphData = [];
+
+  for (let i = 0; i < len; i++) {
+    let color =
+      "#" +
+      Math.random()
+        .toString(16)
+        .substr(-6);
+
+    container = {
+      label: schoolList[i],
+      backgroundColor: color,
+      borderColor: color,
+      borderWidth: 1,
+      data: dataset[schoolList[i]]
+    };
+    graphData.push(container);
+  }
+  showGraph(yearList, graphData, "School Wise Chart", "#SchoolChart");
 }
 
-function generateMajorGraph(yearList, major_name) {
-  var newcontainer = [];
-  var majorContainer = []; //store major name for specific school
-  for (let i = 0; i < yearList.length; i++) {
-    var yearContainer = []; //sperate object by each year
-    for (let j = 0; j < dataLength; j++) {
-      if (data[j].Year == yearList[i] && data[j].School == major_name) {
-        yearContainer.push(data[j]);
-        majorContainer.push(data[j].Major);
-      }
+function processMajorData(strYear, endYear, data) {
+  data = data.message;
+  //major list
+  let majorList = [];
+  data.forEach(element => {
+    if (majorList.indexOf(element._id.School) == -1) {
+      majorList.push(element._id.Major);
     }
-    newcontainer.push(yearContainer);
+  });
+  let yearList = [];
+  for (let i = endYear; i >= strYear; i--) {
+    yearList.push(i);
   }
-  majorContainer = majorContainer.filter(
-    (item, i, ar) => ar.indexOf(item) === i
-  );
-  var result = majorDistribution(newcontainer, majorContainer);
-  showMajorChart(yearList, result, majorContainer);
-}
-function majorDistribution(majorList, majorName) {
-  //create dictionary
-  dict = createDict(majorName);
 
-  yearContainer = [];
+  let dataset = {};
   for (let i = 0; i < majorList.length; i++) {
-    for (let j = 0; j < dict.length; j++) {
-      for (let k = 0; k < majorList[i].length; k++) {
-        if (majorList[i][k].Major == dict[j].major) {
-          dict[j].Student += majorList[i][k].Students;
-        }
-      }
-    }
-    yearContainer.push(dict);
-    dict = createDict(majorName);
+    dataset[majorList[i]] = [];
   }
-  return yearContainer;
-}
 
-function createDict(name) {
-  localDict = [];
-  for (let i = 0; i < name.length; i++) {
-    localDict.push({
-      major: name[i],
-      Student: 0
-    });
-  }
-  return localDict;
-}
-function showMajorChart(year, data, name) {
-  var ctx1 = $("#majorChart");
+  data.forEach(element => {
+    let total = element.total;
+    let major = element._id.Major;
+    dataset[major].push(total);
+  });
+  let len = majorList.length;
+  let graphData = [];
 
-  data = convertToArray(data, name.length);
-  container = [];
-  for (var i = 0; i < data.length; i++) {
-    color =
+  for (let i = 0; i < len; i++) {
+    let color =
       "#" +
       Math.random()
         .toString(16)
-        .substr(-6); //generate random color
-    dataset = {};
-    dataset.label = name[i];
-    dataset.data = data[i];
-    dataset.backgroundColor = color;
-    dataset.borderColor = color;
-    dataset.borderWidth = [1, 1, 1, 1, 1];
-    container.push(dataset);
+        .substr(-6);
+
+    container = {
+      label: majorList[i],
+      backgroundColor: color,
+      borderColor: color,
+      borderWidth: 1,
+      data: dataset[majorList[i]]
+    };
+    graphData.push(container);
   }
-  //pie chart
-  var data1 = {
+  showGraph(yearList, graphData, "Major Wise Chart", "#majorChart");
+}
+
+function showGraph(year, data, label, graphID) {
+  var ctx1 = $(graphID);
+
+  var barChartData = {
     labels: year,
-    datasets: container
+    datasets: data
   };
-  //options
+
   var options = {
     responsive: true,
+    legend: {
+      position: "top"
+    },
     title: {
       display: true,
-      position: "top",
-      text: "No. of Student",
-      fontSize: 18,
-      fontColor: "#111"
+      text: label
     },
-    legend: {
-      display: true,
-      position: "bottom",
-      labels: {
-        fontColor: "#333",
-        fontSize: 16
-      }
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
     }
   };
-  //create Chart class object
-  majorChart = new Chart(ctx1, {
+  // if (chart) {
+  //   chart.destroy();
+  // }
+  chart = new Chart(ctx1, {
     type: "bar",
-    data: data1,
+    data: barChartData,
     options: options
   });
 }
-function convertToArray(data, length) {
-  //create array
-  var container = [];
-  for (let i = 0; i < length; i++) {
-    var iContainer = [];
-    container.push(iContainer);
-  }
-
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].length; j++) {
-      container[j].push(data[i][j].Student);
-    }
-  }
-  return container;
-}
-//test2 branch
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,125 @@
 const express = require("express");
 const router = express.Router();
 
+const Record = require("../model/record");
+
 router.get("/", (req, res) => {
   res.status(200).render("admin");
+});
+
+router.get("/semester", async (req, res) => {
+  var data = req.query;
+  const str = Number(data.str);
+  const end = Number(data.end);
+  var query = [
+    {
+      $match: {
+        Year: {
+          $gte: str,
+          $lte: end
+        }
+      }
+    },
+    {
+      $group: {
+        _id: {
+          Year: "$Year",
+          Semester: "$Semester"
+        },
+        total: {
+          $sum: "$Students"
+        }
+      }
+    },
+    {
+      $sort: {
+        _id: -1
+      }
+    }
+  ];
+
+  var cursor = await Record.aggregate(query);
+  console.log(cursor);
+
+  res.status(200).send({ message: cursor });
+});
+
+router.get("/school", async (req, res) => {
+  var data = req.query;
+  const str = Number(data.str);
+  const end = Number(data.end);
+  var query = [
+    {
+      $match: {
+        Year: {
+          $gte: str,
+          $lte: end
+        }
+      }
+    },
+    {
+      $group: {
+        _id: {
+          Year: "$Year",
+          School: "$School"
+        },
+        total: {
+          $sum: "$Students"
+        }
+      }
+    },
+    {
+      $sort: {
+        _id: -1
+      }
+    }
+  ];
+
+  var cursor = await Record.aggregate(query);
+  console.log(cursor);
+
+  res.status(200).send({ message: cursor });
+});
+
+router.get("/major", async (req, res) => {
+  var data = req.query;
+  const str = Number(data.str);
+  const end = Number(data.end);
+  const school = data.school;
+  console.log(str + " " + end + " " + school);
+
+  var query = [
+    {
+      $match: {
+        Year: {
+          $gte: str,
+          $lte: end
+        },
+        School: school
+      }
+    },
+    {
+      $group: {
+        _id: {
+          Year: "$Year",
+          Major: "$Major"
+        },
+        total: {
+          $sum: "$Students"
+        }
+      }
+    },
+    {
+      $sort: {
+        _id: -1
+      }
+    }
+  ];
+
+  var cursor = await Record.aggregate(query);
+  console.log(cursor);
+
+  res.status(200).send({ message: cursor });
 });
 
 module.exports = router;
